acd2e475283252c965f0b2dacb83e92c21376727
sara niktabe <sara.niktabe@gmail.com>
2023-08-03 22:07:27 +0330

    done

---
+++ 13-Change FavIcon Color/js/script.js
@@ -0,0 +1,8 @@
+const favicon= document.getElementById('favicon');
+
+function toGeen(){
+    favicon.setAttribute('href', 'img/faviconTwo.png')
+}
+function toRed(){
+    favicon.setAttribute('href', 'img/favicon.png')
+}
