fcdc87182a3be4f9ee565abcce67c72d1303715f
sara niktabe <sara.niktabe@gmail.com>
2023-09-03 08:24:14 +0330

    added js code

--- 21-Rotate Page And Show Menu/js/script.js
+++ 21-Rotate Page And Show Menu/js/script.js
@@ -1,11 +1,11 @@
-const open = document.querySelector('#open');
-const close = document.querySelector('#close');
-const container = document.querySelector('.container');
+const open = document.querySelector("#open");
+const close = document.querySelector("#close");
+const container = document.querySelector(".container");
 
-open.addEventListener('click', function(){
-    container.classList.add('show-nav')
-})
+open.addEventListener("click", function () {
+  container.classList.add("show-nav");
+});
 
-close.addEventListener('click', function(){
-    container.classList.remove('show-nav')
-})
+close.addEventListener("click", function () {
+  container.classList.remove("show-nav");
+});
