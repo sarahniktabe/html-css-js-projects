1955c795e0983f9b0abc9edcf549172291a0e83f
sara niktabe <sara.niktabe@gmail.com>
2023-09-08 22:58:49 +0330

    deleted project

--- 23-Page Slider Indicator/js/script.js
+++
@@ -1,8 +0,0 @@
-function imageSlider(anything) {
-  document.querySelector(".bmw").src = anything;
-}
-
-function changeBgColor(color) {
-  const section = document.querySelector(".section");
-  section.style.background = color;
-}
