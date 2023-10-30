c11c3428a03ca46865ecc46827ccd3b4465a7cd2
sara niktabe <sara.niktabe@gmail.com>
2023-10-30 10:16:10 +0330

    added js code

--- 33-/js/script.js
+++ 33-/js/script.js
@@ -1,13 +1,23 @@
 const btn = document.querySelector(".my-btn");
 const alertBox = document.querySelector(".alert-box ");
+let timer;
 
 btn.addEventListener("click", function () {
   showAlertBox();
 });
 
 function showAlertBox() {
+  alertBox.classList.add("show");
+  alertBox.classList.remove("hide");
   if (alertBox.classList.contains("hidden")) {
     alertBox.classList.remove("hidden");
   }
+  timer = setTimeout(function () {
+    hideAlertBox();
+  }, 2000);
 }
 
+function hideAlertBox() {
+  alertBox.classList.remove("show");
+  alertBox.classList.add("hide");
+}
